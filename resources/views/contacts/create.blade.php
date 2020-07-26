d057de4a36bb8c1d32cf6005dac401db45ffef39
Hoang Minh Thang <chethet20@gmail.com>
2020-07-27 00:36:42 +0700

    First commit

---
+++ resources/views/contacts/create.blade.php
@@ -0,0 +1,47 @@
+@extends('contacts.layout')
+  
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Create New Contact</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('contacts.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+   
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Warning!</strong> Please check your input code<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+   
+<form action="{{ route('contacts.store') }}" method="POST">
+    @csrf
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                <input type="text" name="name" class="form-control" placeholder="Name">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Information:</strong>
+                <textarea class="form-control" style="height:280px" name="information" placeholder="Information"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary mb-3">Submit</button>
+        </div>
+    </div>
+   
+</form>
+@endsection
---
+++ resources/views/contacts/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Layout contact</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<div class="jumbotron text-center">
+  <h1>CONTACT BOOK</h1>
+  <p>Code with Thang</p> 
+</div>
+<div class="container">
+    @yield('content')
+</div>
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>@CodewithThang</p>
+</div>
+</body>
+</html>
